6c071b766ede7f2b33bccda7b32293a2c143bdf0
Jesús Terino Rodriguez <157137273+TERINO-73@users.noreply.github.com>
2026-01-20 19:29:33 +0100

    Corrección problemas locales

---
+++ dev-personal-app/src/main/java/com/jesusterino/app/DevPersonalAppApplication.java
@@ -0,0 +1,12 @@
+package com.jesusterino.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPersonalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevPersonalAppApplication.class, args);
+    }
+}
